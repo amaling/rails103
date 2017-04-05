c6fe4fa12d4727eb6429a73b531b95f26bf176d2
amaling <amaling123@gmail.com>
2017-04-05 18:11:36 +0800

    add user_id to group

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -17,15 +17,16 @@ class GroupsController < ApplicationController
    @group = Group.new
  end
 
-   def create
-     @group = Group.new(group_params)
+ def create
+   @group = Group.new(group_params)
+   @group.user = current_user
 
-     if @group.save
-       redirect_to groups_path
-     else
-       render :new
-     end
+   if @group.save
+     redirect_to groups_path
+   else
+     render :new
    end
+ end
 
 
      def update
